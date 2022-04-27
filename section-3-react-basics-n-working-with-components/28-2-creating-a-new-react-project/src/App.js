863c8f239c8657146865f8a2ed9cf7dc29a732d7
Ayon Al Mamun <ayonalmamun@gmail.com>
2022-04-27 10:51:11 +0600

    section 3 - tutorial 32 done

--- section-3-react-basics-n-working-with-components/28-2-creating-a-new-react-project/src/App.js
+++ section-3-react-basics-n-working-with-components/28-2-creating-a-new-react-project/src/App.js
@@ -1,7 +1,13 @@
 function App() {
+  // regular js
+  const para = document.createElement('p');
+  para.textContent = 'This is also visible too';
+  document.getElementById('root').append(para);
+  // regular js
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visible</p>
     </div>
   );
 }
