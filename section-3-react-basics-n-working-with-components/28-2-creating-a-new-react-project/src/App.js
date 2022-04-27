741d5df2b44ecbc4a3e9f60f4b3e204c6654f5ea
Ayon Al Mamun <ayonalmamun@gmail.com>
2022-04-27 11:02:33 +0600

    section 3 - tutorial 33 done

--- section-3-react-basics-n-working-with-components/28-2-creating-a-new-react-project/src/App.js
+++ section-3-react-basics-n-working-with-components/28-2-creating-a-new-react-project/src/App.js
@@ -1,13 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
-  // regular js
-  const para = document.createElement('p');
-  para.textContent = 'This is also visible too';
-  document.getElementById('root').append(para);
-  // regular js
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visible</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
