03b9b80c4e9c16b17207200310ca311ce00d8695
Ayon Al Mamun <ayonalmamun@gmail.com>
2022-07-29 16:13:01 +0600

    139 done

---
+++ section-11-practice-project-building-a-food-order-app/project/src/components/Cart/Cart.js
@@ -0,0 +1,24 @@
+import classes from "Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: 1, name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>item.name</li>
+      ))}
+    </ul>
+  );
+  return (
+    <div>
+      {cartItmes}
+      <div className={classes.total}>Total Amount</div>
+      <div>35.23</div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
