bc01b16f90ccafc5fbc99c6e5e019e89e530fc17
Ayon Al Mamun <ayonalmamun@gmail.com>
2022-07-30 22:41:48 +0600

    bug fixing: 146

--- section-11-practice-project-building-a-food-order-app/project/src/components/Cart/Cart.js
+++ section-11-practice-project-building-a-food-order-app/project/src/components/Cart/Cart.js
@@ -4,6 +4,7 @@ import CartContext from "../../store/cart-context";
 import Modal from "../UI/Modal";
 
 import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
@@ -11,10 +12,21 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        ></CartItem>
       ))}
     </ul>
   );
